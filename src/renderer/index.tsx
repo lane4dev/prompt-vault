cb73b2bcb3902f03773d842772f05d0a43d5ef73
lane4dev <dev@tangyu.me>
2025-11-30 15:06:26 +0800

    feat: implement ThemeProvider for managing application theme with local storage support

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,12 +1,16 @@
 import ReactDom from 'react-dom/client'
 import React from 'react'
 
+import { ThemeProvider } from './components/theme-provider'
+
 import { AppRoutes } from './routes'
 
 import './globals.css'
 
 ReactDom.createRoot(document.querySelector('app') as HTMLElement).render(
   <React.StrictMode>
-    <AppRoutes />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <AppRoutes />
+    </ThemeProvider>
   </React.StrictMode>
 )
